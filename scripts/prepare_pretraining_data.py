fd3a8a9a8ebd544a111e954aa82d793a05d5e382
Yixuan Ni <u5552827@anu.edu.au>
2017-10-03 17:28:43 +1100

    Fix indentation

--- scripts/prepare_pretraining_data.py
+++ scripts/prepare_pretraining_data.py
@@ -1,7 +1,7 @@
 # Prepare pretraining data by duplicate sentences.
 #
 # Usage:
-# python prepare_training_data.py --input $INPUT_PATH --output $OUTPUT_PATH
+# python prepare_pretraining_data.py $INPUT_PATH $OUTPUT_PATH
 #
 
 
@@ -16,13 +16,13 @@ def make_pre(input_file, output_file):
         for p in pairs:
             f.write(p[0] + "\t" + p[1] + "\n")
     print ("Finish writing file.")
-	
+    
 def main(argv):
-	make_pre(argv.input, argv.output)
+    make_pre(argv.input, argv.output)
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description="Prepare pretraining data")
-	parser.add_argument("input", default="../data/billion/news.2011.en.shuffled")
-	parser.add_argument("output", default="../data/billion/billion2011.txt")
-	args = parser.parse_args()
-	main(args)
+    parser = argparse.ArgumentParser(description="Prepare pretraining data")
+    parser.add_argument("input", default="../data/billion/news.2011.en.shuffled")
+    parser.add_argument("output", default="../data/billion/billion2011.txt")
+    args = parser.parse_args()
+    main(args)
--- scripts/prepare_training_data.py
+++ scripts/prepare_training_data.py
@@ -1,7 +1,7 @@
 # Prepare training data from json file.
 #
 # Usage:
-# python prepare_pretraining_data.py --input $INPUT_PATH --output $OUTPUT_PATH
+# python prepare_training_data.py $INPUT_PATH $OUTPUT_PATH [-p]
 #
 
 import json
@@ -10,22 +10,22 @@ import codecs
 
 
 def read_file(input_file):
-	with open(input_file, 'r') as f:
-		data = json.load(f)
-		print ("Finish reading file.")
-		return data
+    with open(input_file, 'r') as f:
+        data = json.load(f)
+        print ("Finish reading file.")
+        return data
 
 def sent2seq(sentences, output_file):
-	with codecs.open(output_file, 'w', encoding='utf8') as f:
-		for s in sentences:
-			input_sentence = " ".join(s["input_sentence"])
-			corrected_sentence =  " ".join(s["corrected_sentence"]) 
+    with codecs.open(output_file, 'w', encoding='utf8') as f:
+        for s in sentences:
+            input_sentence = " ".join(s["input_sentence"])
+            corrected_sentence =  " ".join(s["corrected_sentence"]) 
             if args.preprocess:
                 input_sentence = " ".join(s["input_sentence"][::-1])
                 input_sentence = filter_digits(input_sentence)
                 corrected_sentence = filter_digits(corrected_sentence)
-			f.write(input_sentence + "\t" + corrected_sentence + "\n")
-	print ("Finish writing file at ") + output_file + "."
+            f.write(input_sentence + "\t" + corrected_sentence + "\n")
+    print ("Finish writing file at ") + output_file + "."
 
 def filter_digits(sentence):
     """
@@ -33,17 +33,17 @@ def filter_digits(sentence):
     """
     import re
     return re.sub("\d", "d", sentence)
-	
+    
 def main(argv):
-	d = read_file(argv.input)	
-	sent2seq(d["data"], argv.output)
-	
+    d = read_file(argv.input)   
+    sent2seq(d["data"], argv.output)
+    
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description="convert .json file into .txt file")
-	parser.add_argument("input", default="nucle_validation_json.json")
-	parser.add_argument("output", default="nucle_validation.txt")
-    parser.add_argument("--preprocess", "-p", des="preprocess", action="store_true",
+    parser = argparse.ArgumentParser(description="convert .json file into .txt file")
+    parser.add_argument("input", default="nucle_train_json.json")
+    parser.add_argument("output", default="data.txt")
+    parser.add_argument("--preprocess", "-p", dest="preprocess", action="store_true",
                         default = False, 
                         help = "If set true, reverse the order of input sentences and replace all the digits.")
-	args = parser.parse_args()
-	main(args)
+    args = parser.parse_args()
+    main(args)
