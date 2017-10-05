cd60bf1016ee9cf2f0e985de343c8079080079fa
Yixuan Ni <ni006@bracewell-login.cm.hpc>
2017-10-06 02:46:02 +1100

    fix bugs

--- seq2seq/evaluator/predictor.py
+++ seq2seq/evaluator/predictor.py
@@ -33,7 +33,7 @@ class Predictor(object):
         """
         src_id_seq = Variable(torch.LongTensor([self.src_vocab.stoi[tok] for tok in src_seq]),
                               volatile=True).view(1, -1) 
-        decoder_kick = Variable(torch.LongTensor([self.tgt_vocab.stoi['<sos>']]),
+        decoder_kick = Variable(torch.LongTensor([self.tgt_vocab.stoi['<sos>']]))
         if torch.cuda.is_available():
             src_id_seq = src_id_seq.cuda()
             decoder_kick = decoder_kick.cuda()
@@ -47,9 +47,11 @@ class Predictor(object):
                               encoder_hidden=encoder_hidden,
                               encoder_outputs=encoder_outputs,
                               function=self.model.decode_function,
+                              error_index=error_index,
+                              src_id_seq=src_id_seq)
+        else:
+            softmax_list,_,other = self.model(src_id_seq, [len(src_seq)], decoder_kick)
         length = other['length'][0]
-
-        #tgt_id_seq = [other['sequence'][di][0].data[0] for di in range(length)]
         tgt_id_seq = []
         for di in range(len(src_seq)):
             tgt_id_seq.append(other['sequence'][di][0].data[0])
